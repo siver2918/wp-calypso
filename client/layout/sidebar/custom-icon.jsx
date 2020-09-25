71af3a9168112e648a2e97205f1e3840b30b49d2
cpap <papazoglou.charalampos@gmail.com>
2020-09-25 12:07:02 +0300

    Fixes jetpack fallback icon

--- client/layout/sidebar/custom-icon.jsx
+++ client/layout/sidebar/custom-icon.jsx
@@ -1,8 +1,7 @@
 /**
  * SidebarCustomIcon -
  *   Renders an <img> tag with props supplied if icon is a data image
- *   or a dashicon in other case. If icon is not supplied or undefined
- *   returns a blank SVG
+ *   or a dashicon in other case.
  *   Adds className="sidebar__menu-icon" to the supplied className.
  *
  *   Purpose: To display a custom icon in the sidebar when using a
@@ -14,23 +13,12 @@
  */
 import React from 'react';
 
-const blankSvg =
-	"data:image/svg+xml;charset=utf8,%3Csvg%20xmlns='http://www.w3.org/2000/svg'%3E%3C/svg%3E";
-const blankSvgStyle = { height: '20px', width: '20px' };
-
 const SidebarCustomIcon = ( { alt, className, icon, ...rest } ) => {
 	if ( ! icon ) {
-		return (
-			<img
-				alt={ alt || '' }
-				className={ 'sidebar__menu-icon ' + ( className || '' ) }
-				src={ blankSvg }
-				style={ blankSvgStyle }
-				{ ...rest }
-			/>
-		);
+		return null;
 	}
-	if ( icon.indexOf( 'data:image' ) === 0 ) {
+
+	if ( 0 !== icon.indexOf( 'dashicons-' ) ) {
 		return (
 			<img
 				alt={ alt || '' }
